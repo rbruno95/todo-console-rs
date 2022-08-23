151dc55eb718f3d70dffa2ea97ee873fba737624
Bruno <48922045+rbruno95@users.noreply.github.com>
2022-08-23 17:19:45 -0400

    implement new command

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use std::io::{self, Write};
 use std::process;
 
+struct Task {
+    name: String,
+    done: bool,
+}
+
 fn main() {
     println!("Welcome to your TODO app");
     println!("If you need help or if you don't remember the commands type help");
+
+    let mut tasks = Vec::new();
 
     loop {
         print!(">>> ");
@@ -21,7 +28,7 @@ fn main() {
             "exit" => exit(),
             "help" => help(),
             "list" => list(),
-            "new" => new(),
+            "new" => new(&mut tasks),
             "update" => update(),
             _ => invalid_command(&command[..]),
         };
@@ -49,7 +56,19 @@ update: interface to edit a task
 
 fn list() {}
 
-fn new() {}
+fn new(tasks: &mut Vec<Task>) {
+    print!("Enter task name: ");
+
+    io::stdout().flush().unwrap();
+
+    let mut name = String::new();
+
+    io::stdin().read_line(&mut name).unwrap();
+
+    name = name.trim().to_string();
+
+    tasks.push(Task { name, done: false });
+}
 
 fn update() {}
 

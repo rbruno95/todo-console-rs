ae5e6b73f80fbb66cae6299dbf6e9b67f0794018
Bruno <48922045+rbruno95@users.noreply.github.com>
2022-08-23 16:45:49 -0400

    add skeleton for commands management

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to your TODO app");
+    println!("If you need help or if you don't remember the commands type help");
+
+    loop {
+        print!(">>> ");
+
+        io::stdout().flush().unwrap();
+
+        let mut command = String::new();
+
+        io::stdin().read_line(&mut command).unwrap();
+
+        match &command[..] {
+            "exit" => exit(),
+            "delete" => delete(),
+            "list" => list(),
+            "new" => new(),
+            "update" => update(),
+            _ => invalid_command(),
+        };
+    }
 }
+
+fn new() {}
+
+fn exit() {}
+
+fn list() {}
+
+fn update() {}
+
+fn delete() {}
+
+fn invalid_command() {}

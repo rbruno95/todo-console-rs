579050fbd7bd79f186fac8da8587407c124418ab
Bruno <48922045+rbruno95@users.noreply.github.com>
2022-08-24 12:45:12 -0400

    move commands funtion to a separated file

---
+++ src/commands.rs
@@ -0,0 +1,128 @@
+use std::io::{self, Write};
+use std::process;
+
+pub struct Task {
+    pub name: String,
+    pub done: bool,
+}
+
+pub fn delete(tasks: &mut Vec<Task>) {
+    if tasks.len() == 0 {
+        println!("You don't have tasks.");
+        return;
+    }
+
+    for (i, task) in tasks.iter().enumerate() {
+        println!("{i} -> {}", task.name);
+    }
+
+    print!("Enter the number of the task: ");
+
+    io::stdout()
+        .flush()
+        .expect("There was a problem doing flush.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("There was a problem reading the index.");
+
+    let index = index
+        .trim()
+        .parse::<usize>()
+        .expect("There was a problem parsing the index to usize.");
+
+    if tasks.len() > index {
+        tasks.remove(index);
+    } else {
+        println!("Select one of the listed numbers.");
+    }
+}
+
+pub fn exit() {
+    process::exit(0);
+}
+
+pub fn help() {
+    println!(
+        r#"
+delete: interface to delete a task
+exit: exit the program
+help: show the available commands
+list: show all the tasks
+new: interface to create a task
+update: interface to edit a task
+        "#
+    );
+}
+
+pub fn list(tasks: &mut Vec<Task>) {
+    println!("TODO:");
+
+    for task in tasks.iter().filter(|task| !task.done) {
+        println!("[ ] {}", task.name);
+    }
+
+    println!("DONE:");
+
+    for task in tasks.iter().filter(|task| task.done) {
+        println!("[X] {}", task.name);
+    }
+}
+
+pub fn new(tasks: &mut Vec<Task>) {
+    print!("Enter task name: ");
+
+    io::stdout()
+        .flush()
+        .expect("There was a problem doing flush.");
+
+    let mut name = String::new();
+
+    io::stdin()
+        .read_line(&mut name)
+        .expect("There was a problem reading the name.");
+
+    name = name.trim().to_string();
+
+    tasks.push(Task { name, done: false });
+}
+
+pub fn update(tasks: &mut Vec<Task>) {
+    if tasks.len() == 0 {
+        println!("You don't have tasks.");
+        return;
+    }
+
+    for (i, task) in tasks.iter().enumerate() {
+        println!("{i} -> {}", task.name);
+    }
+
+    print!("Enter the number of the task: ");
+
+    io::stdout()
+        .flush()
+        .expect("There was a problem doing flush.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("There was a problem reeading the index.");
+
+    let index = index
+        .trim()
+        .parse::<usize>()
+        .expect("There was a problem parsing.");
+
+    if tasks.len() > index {
+        tasks[index].done = !tasks[index].done;
+    } else {
+        println!("Select one of the listed numbers.");
+    }
+}
+
+pub fn invalid_command(command: &str) {
+    println!("The command {command} is invalid.");
+}
